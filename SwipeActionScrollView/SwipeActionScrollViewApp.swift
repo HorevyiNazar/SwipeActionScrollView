82c8a5747cdc9b5fe12feb6823af8ea813bf3f74
HorevyiNazar <goreviynazar@gmail.com>
2024-02-17 12:23:26 +0100

    Initial Commit

---
+++ SwipeActionScrollView/SwipeActionScrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeActionScrollViewApp.swift
+//  SwipeActionScrollView
+//
+//  Created by Назар Горевой on 17/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwipeActionScrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
